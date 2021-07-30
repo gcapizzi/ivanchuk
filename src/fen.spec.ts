b65661174ca85e68a15c3384a20d2f43b52cb714
Giuseppe Capizzi <g.capizzi@gmail.com>
2021-07-30 22:35:32 +0200

    Add better FEN test

--- src/fen.spec.ts
+++ src/fen.spec.ts
@@ -5,5 +5,18 @@ describe("parse", () => {
   it("parses a position in FEN format", () => {
     expect(fen.parse("8/8/8/8/8/8/8/8")?.equals(chess.Game.empty())).toBe(true);
     expect(fen.parse("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR")?.equals(chess.Game.startingPosition())).toBe(true);
+    expect(
+      fen
+        .parse("rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R")
+        ?.equals(
+          chess.Game.startingPosition()
+            .removePiece(chess.Square.fromString("e2")!)
+            .addPiece(chess.Piece.fromString("P")!, chess.Square.fromString("e4")!)
+            .removePiece(chess.Square.fromString("c7")!)
+            .addPiece(chess.Piece.fromString("p")!, chess.Square.fromString("c5")!)
+            .removePiece(chess.Square.fromString("g1")!)
+            .addPiece(chess.Piece.fromString("N")!, chess.Square.fromString("f3")!)
+        )
+    ).toBe(true);
   });
 });
